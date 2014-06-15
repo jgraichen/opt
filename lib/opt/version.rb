5e536c05475ac027c34142ecde792e8d071beaa4
Jan Graichen <jg@altimos.de>
2014-06-15 14:49:12 +0200

    Release 0.2

--- lib/opt/version.rb
+++ lib/opt/version.rb
@@ -1,3 +1,13 @@
 module Opt
-  VERSION = "0.1.1"
+  module VERSION
+    MAJOR = 0
+    MINOR = 2
+    PATCH = 0
+    STAGE = nil
+    STRING = [MAJOR, MINOR, PATCH, STAGE].reject(&:nil?).join('.').freeze
+
+    def self.to_s
+      STRING
+    end
+  end
 end
