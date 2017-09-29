4189999c25930f1160c84e9316ed0e65cf991716
Jan Graichen <jgraichen@altimos.de>
2017-09-29 14:43:30 +0200

    Fix rspec config
    * Remove code climate test reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,22 +1,16 @@
 require 'rspec'
 
 if ENV['CI'] || ENV['COVERAGE']
-  require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
-
   require 'coveralls'
   Coveralls.wear! do
     add_filter 'spec'
   end
 end
 
-# Load stic
 require 'opt'
 
-# Load spec support files
 Dir[File.expand_path('spec/support/**/*.rb')].each {|f| require f }
 
 RSpec.configure do |config|
-  # Random order
   config.order = 'random'
 end
